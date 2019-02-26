06409890f7ab6f76023b56474730f4945c0ddf2c
Van Tien Sy <sy.vantien@mobifone.vn>
2019-02-26 13:54:52 +0700

    cuongdq push by sh

--- github-push.sh
+++ github-push.sh
@@ -4,5 +4,5 @@ git commit -m "cuongdq push by sh"
 #git remote -v
 #git remote remove github 
 #(thay chu origin = github de quan ly de hon)
-git remote add github https://github.com/syvt/node-server-demo.git
+#git remote add github https://github.com/syvt/node-server-demo.git
 git push -u github master
